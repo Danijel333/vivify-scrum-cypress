162239a72afc062bd9522288b55c08cb6039f1c6
Danijel Janjic <danijel@Danijels-MacBook-Pro.local>
2022-07-14 15:05:02 +0200

    board pom i spec files created and done for now

---
+++ cypress/page_object/board.js
@@ -0,0 +1,66 @@
+class Board {
+
+    get addNewButton() {
+        return cy.contains('Add New')
+    }
+
+    get addBoardButton() {
+        return cy.contains('Add Board')
+    }
+
+    get openOrganizationDropdown(){
+        return cy.get('div[class="el-select vs-c-new-board-select"]');
+    }
+
+    get selectOrganization(){
+        return cy.get('ul[class="el-scrollbar__view el-select-dropdown__list"]').children().first();
+    }
+
+    get boardTitleInput() {
+        return cy.get('input[name="name"]');
+    }
+
+    get goNextButton() {
+        return cy.get('button[name="next_btn"]')
+    }
+
+    get boardTypeRadioButton() {
+        return cy.get('span[class="vs-c-radio-check"]').first();
+    }
+
+    get finishButton(){
+        return cy.get('button[class="el-button vs-c-button-focus el-button--success el-button--large"]').last();
+    }
+
+    addNewBoard(title){
+        cy.intercept({
+            method : 'POST',
+            url : "boards"
+        }).as('boardCreated');
+
+        this.addNewButton.click();
+        this.addBoardButton.click();
+        this.openOrganizationDropdown.click();
+        this.selectOrganization.click();
+        this.boardTitleInput.type(title);
+        this.goNextButton.click();
+        this.boardTypeRadioButton.click();
+        this.goNextButton.click();
+        this.goNextButton.click();
+        this.goNextButton.click();
+        this.finishButton.click();
+
+        cy.wait('@boardCreated').then(interception => {
+            expect(interception.response.statusCode).eql(201);
+            expect(interception.response.statusMessage).eql('Created');
+        })
+    };
+
+    addBoardWithoutTitle(){
+        this.addNewButton.click();
+        this.addBoardButton.click();
+        this.goNextButton.should('be.disabled');
+    }
+}
+
+export const createBoard = new Board();
