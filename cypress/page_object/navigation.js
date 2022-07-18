d47a77d0028bd23b66a7c7d197cece1f97b196d5
Ivana Komatina <ivanakomatina@Ivanas-MacBook-Air.local>
2022-07-18 12:14:47 +0200

    Added module.exports and small changes in pom. Add Clients finished

--- cypress/page_object/navigation.js
+++ cypress/page_object/navigation.js
@@ -1,24 +1,43 @@
-class Navigation{
+module.exports = {
 
     get nextBtn() {
         return cy.get('button[name="next_btn"]')
-    }
+    },
 
     get configurationBtn() {
         return cy.get('a[class="vs-c-site-logo"]').last()
-    }
+    },
 
     get deleteBtn() {
         return cy.get('button[class="vs-c-btn vs-c-btn--warning vs-c-btn--spaced"]')
-    }
+    },
 
-    get boardOkBtn() {
+    get okBtn() {
         return cy.get('button[class="vs-c-btn vs-c-btn--primary vs-c-btn--lg vs-u-font-sm vs-c-modal--features-confirm-button"]')
-    }
+    },
 
     get yesBtn() {
         return cy.get('button[name="save-btn"]')
+    },
+
+    get clientBtn() {
+        return cy.get('a[class="vs-c-site-logo"]').eq(2)
+    },
+
+    get confirmYourActionTitle() {
+        return cy.get('h4').contains('Confirm Your Action')
+    },
+
+    get saveBtn() {
+        return cy.get('button').contains('Save')
+    },
+
+    get exitModal() {
+        return cy.get('button[name="close-confirmation-modal-btn"]')
+    },
+
+    navigationClient() {
+        this.clientBtn.click()
+        this.okBtn.click()
     }
 }
-
-export const navigation = new Navigation()
