3a6bc743642a3ddb3d7268bfcbddc376b669bc25
Danijel Janjic <danijel@Danijels-MacBook-Pro.local>
2022-07-21 08:28:46 +0200

    get board data api done

--- cypress/e2e/API/boardApi.cy.js
+++ cypress/e2e/API/boardApi.cy.js
@@ -7,32 +7,52 @@ describe("Create board", () => {
         login.post({assert : false});
     })
 
-    it("Test 1 - Create board with valid data", () => {
-        board.post({});
-    })
-
-    it("Test 2 - Create board in different organization", () => {
-        board.post({organization_id : 20303});
-    })
-
-    it("Test 3 - Create board valid data - scrum board", () => {
-        board.post({type : "scrum_board"});
-    })
-
-    it("Test 4 - Create board without name", () => {
+    it("Test 1 - Create board without name", () => {
         board.post({name : "", statusCode : 422, statusText: "Unprocessable Entity"});
     })
 
-    it("Test 5 - Create board without type", () => {
+    it("Test 2 - Create board without type", () => {
         board.post({type : "", statusCode : 422, statusText: "Unprocessable Entity"});
     })
 
-    it("Test 6 - Create board with invalid board type", () => {
+    it("Test 3 - Create board with invalid board type", () => {
         board.post({type : "some_board", statusCode : 422, statusText: "Unprocessable Entity"});
     })
 
-    it("Test 7 - Create board without logging in", () => {
+    it("Test 4 - Create board without logging in", () => {
         board.post({token : "", statusCode : 401, statusText: "Unauthorized"});
+    })
+
+    it("Test 5 - Create board with valid data", () => {
+        board.post({});
+    })
+
+    it("Test 6 - Create board in different organization", () => {
+        board.post({organization_id : 20303});
+    })
+
+    it("Test 7 - Create board valid data - scrum board", () => {
+        board.post({type : "scrum_board"});
+    })
+
+    it("Test 8 - Get non-existing board data", () => {
+        board.get({boardId : 99999, statusCode : 404, statusText : "Not Found"});
+    })
+
+    it("Test 9 - Get other user board data", () => {
+        board.get({boardId : 7945, statusCode : 403, statusText : "Forbidden"});
+    })
+
+    it("Test 10 - Get other user board data with permission", () => {
+        board.get({boardId : 7943});
+    })
+
+    it("Test 11 - Get deleted board data", () => {
+        board.get({boardId : 10252, statusCode : 404, statusText : "Not Found"});
+    })
+
+    it("Test 12 - Get board data", () => {
+        board.get({});
     })
 
 
