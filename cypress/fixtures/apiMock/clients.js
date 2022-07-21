a67c09ba85ce8c9ec8b0c784e692f97017fbbe73
Ivana Komatina <ivanakomatina@Ivanas-MacBook-Air.local>
2022-07-21 12:43:23 +0200

    ClientsApi finished

---
+++ cypress/fixtures/apiMock/clients.js
@@ -0,0 +1,104 @@
+module.exports = {
+
+    post({
+
+        name = 'Allen Brown',
+        email = 'Kassandra4@hotmail.com',
+        phone = '961-770-7727',
+        address = "5786 Little Summit",
+        organizationID = "",
+        statusCode = 200,
+        statusText = "OK"
+    }) {
+        return cy.request({
+            failOnStatusCode : false,
+            method : 'POST',
+            url : `${Cypress.env('baseAPI')}organizations/${organizationID}/clients`,
+            headers : {
+                "Authorization": 'Bearer ' +  window.localStorage.getItem('token'),
+                "Accept" : 'application/json'
+            },
+            body : {
+               name: name,
+               email: email,
+               phone: phone,
+               address: address
+            }
+        }).then(response => {
+            expect(response.status).eql(statusCode);
+            expect(response.statusText).eql(statusText);
+        })
+    },
+
+    get({
+
+        organizationID = "",
+        statusCode = 200,
+        statusText = "OK"
+    }) {
+        return cy.request({
+            failOnStatusCode : false,
+            method : 'GET',
+            url : `${Cypress.env('baseAPI')}organizations/${organizationID}/clients?page=1`,
+            headers : {
+                "Authorization": 'Bearer ' +  window.localStorage.getItem('token'),
+                "Accept" : 'application/json'
+            },
+        }).then(response => {
+            expect(response.status).eql(statusCode);
+            expect(response.statusText).eql(statusText);
+        })
+    },
+
+    put({
+
+        name = 'Alice Peter',
+        email = 'Kassandra4@hotmail.com',
+        phone = '961-770-7727',
+        address = "5786 Little Summit",
+        organizationID = "",
+        clientID = "",
+        statusCode = 200,
+        statusText = "OK"
+    }) {
+        return cy.request({
+            failOnStatusCode : false,
+            method : 'PUT',
+            url : `${Cypress.env('baseAPI')}organizations/${organizationID}/clients/${clientID}`,
+            headers : {
+                "Authorization": 'Bearer ' +  window.localStorage.getItem('token'),
+                "Accept" : 'application/json'
+            },
+            body : {
+                name: name,
+                email: email,
+                phone: phone,
+                address: address
+             }
+        }).then(response => {
+            expect(response.status).eql(statusCode);
+            expect(response.statusText).eql(statusText);
+        })
+    },
+
+    delete({
+
+        organizationID = "",
+        clientID = "",
+        statusCode = 200,
+        statusText = "OK"
+    }) {
+        return cy.request({
+            failOnStatusCode : false,
+            method : 'DELETE',
+            url : `${Cypress.env('baseAPI')}organizations/${organizationID}/clients/${clientID}`,
+            headers : {
+                "Authorization": 'Bearer ' +  window.localStorage.getItem('token'),
+                "Accept" : 'application/json'
+            }
+        }).then(response => {
+            expect(response.status).eql(statusCode);
+            expect(response.statusText).eql(statusText);
+        })
+    }
+}
